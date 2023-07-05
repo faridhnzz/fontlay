e6aa4ed97a04a243e569d6c4c794dc61341be3f9
faridhnzz <faridsaja12@gmail.com>
2023-07-05 20:54:24 +0700

    feat: set port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,13 @@ module.exports = {
     {
       name: 'fontlay-web',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start',
+      args: 'start -p 3001',
       instances: 2,
       watch: false,
       autorestart: true,
       env: {
         NODE_ENV: 'production',
+        PORT: 3001,
       },
       env_development: {
         NODE_ENV: 'development',
